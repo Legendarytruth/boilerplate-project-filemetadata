9e44a9a0bf9f62baedff50b3628fc5f2d5780e08
Legendarytruth <65697867+Legendarytruth@users.noreply.github.com>
2021-07-05 02:34:19 -0400

    Finished Assignment
    Done

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+var multer = require("multer");
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(multer().single("upfile"));
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post("/api/fileanalyse", (req, res) => {
+  //console.log(req.file +" / "+ req.file.originalname +" / "+ req.file.mimetype +" / "+ req.file.size);
+  res.send({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
 
 const port = process.env.PORT || 3000;
